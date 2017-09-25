55e11da5c6d1a524a88b2427fab2a71f0ab7a422
NISH1001 <nishanpantha@gmail.com>
2017-09-25 15:09:35 +0545

    Fix segmented reply

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import questionparser
 from textprocessor import preprocess, convert_tuples_to_string
 from dataloader import load_data
 from markov import MarkovChain
+from segmenter import segment_words
 
 class Bot:
     def __init__(self, markov_chain):
@@ -24,16 +25,22 @@ class Bot:
             try:
                 you = input("You >> ")
                 words = questionparser.process(you)
-                bot = ""
-                for keywords in questionparser.generate_variations(words):
-                    words = self.generate_reply(keywords, reply_len=15)
-                    if len(words) == 1 and words[0] == keywords:
-                        bot = "??? I can't know everything which is the beauty of life itself. ???"
-                    else:
-                        bot = convert_tuples_to_string(words)
-                        break
-                print("Paradox >> {}".format(bot))
-            except KeyboardInterrupt:
+                segments = segment_words(words, segment_size = 3, overlap = 1)
+                replies = []
+                for segment in segments:
+                    print("Current segment : {}".format(segment))
+                    bot = ""
+                    for keywords in questionparser.generate_variations(segment):
+                        print("Variation : {}".format(keywords))
+                        reply = self.generate_reply(keywords, reply_len=4)
+                        print(reply)
+                        if len(reply) == 1 and reply[0] == keywords:
+                            bot = "??? I can't know everything which is the beauty of life itself. ???"
+                        else:
+                            bot = convert_tuples_to_string(reply)
+                            break
+                    print("Paradox >> {}".format(bot))
+            except (KeyboardInterrupt, EOFError) as e:
                 print("Bye. Cheers. Stay awesome...")
                 break
 
--- segmenter.py
+++ segmenter.py
@@ -13,6 +13,18 @@ def segment_text(text, segment_size=3, overlap=2):
             segments.append(tuple(word))
     return segments
 
+def segment_words(words, segment_size=3, overlap=2):
+    """
+        Segment the given text into  n segments
+    """
+    segments = []
+    start = overlap if overlap > 0 else 0
+    for i in range(start, len(words), segment_size - overlap):
+        word = words[i - overlap : i + segment_size - overlap]
+        if word:
+            segments.append(tuple(word))
+    return segments
+
 def main():
     text = "what are you doing in your life"
     segments = segment_text(text, segment_size = 4, overlap = 2)
