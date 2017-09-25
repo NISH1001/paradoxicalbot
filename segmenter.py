e82fc247827604c927c7414463e8078e999e0a35
NISH1001 <nishanpantha@gmail.com>
2017-09-25 19:38:39 +0545

    Fix text segmentation when length is less than the segment

--- segmenter.py
+++ segmenter.py
@@ -5,21 +5,13 @@ def segment_text(text, segment_size=3, overlap=2):
         Segment the given text into  n segments
     """
     words = text.split()
-    if len(words) == 1:
-        return [ tuple(words) ]
-    segments = []
-    start = overlap if overlap > 0 else 0
-    for i in range(start, len(words), segment_size - overlap):
-        word = words[i - overlap : i + segment_size - overlap]
-        if word:
-            segments.append(tuple(word))
-    return segments
+    return segment_words(words, segment_size, overlap)
 
 def segment_words(words, segment_size=3, overlap=2):
     """
         Segment the given text into  n segments
     """
-    if len(words) == 1:
+    if len(words) == 1 or len(words) < segment_size:
         return [ tuple(words) ]
     segments = []
     start = overlap if overlap > 0 else 0
@@ -30,7 +22,7 @@ def segment_words(words, segment_size=3, overlap=2):
     return segments
 
 def main():
-    text = "humans"
+    text = "i"
     segments = segment_text(text, segment_size = 4, overlap = 2)
     #segments = segment_words(text.split(), segment_size = 4, overlap = 2)
     print(segments)
