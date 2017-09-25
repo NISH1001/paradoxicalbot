ddf47102dfed4cbeece3aa846b80322acd885469
NISH1001 <nishanpantha@gmail.com>
2017-09-25 15:40:38 +0545

    Fix reply length according to size of the segment

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import random
-import re
+"""
+    The core module for the bot
+"""
 
 import questionparser
 from textprocessor import preprocess, convert_tuples_to_string
@@ -10,15 +11,49 @@ from markov import MarkovChain
 from segmenter import segment_words
 
 class Bot:
+    """
+        The actual Bot based on the Markov Chain.
+    """
+
     def __init__(self, markov_chain):
         if not markov_chain:
             raise ValueError("No MarkovChain supplied...")
         self.markov_chain = markov_chain
 
     def generate_reply(self, start, reply_len=10):
+        """
+            Generate a reply with markov chain process
+        """
         words_generated = [ word for word in self.markov_chain.generate(start, reply_len) ]
         return words_generated
-        #return convert_tuples_to_string(words_generated)
+
+    def generate_segmented_reply(self, segment, reply_len=3):
+        """
+            Generate reply for the given segment.
+            Every possible variations are generated for the segment
+            to compensate for the grammar.
+
+            Eg:
+                doing i am => i am doing, i doing am, doing am i
+        """
+        bot = ""
+        for keywords in questionparser.generate_variations(segment):
+            reply = self.generate_reply(keywords, reply_len)
+            if len(reply) == 1 and reply[0] == keywords:
+                continue
+            else:
+                bot = convert_tuples_to_string(reply)
+                break
+        return bot
+
+    def get_reply_len_for_segment(self, segment):
+        length = len(segment)
+        if length == 1:
+            return 10
+        elif length == 2:
+            return 5
+        else:
+            return 3
 
     def run(self):
         while True:
@@ -27,19 +62,14 @@ class Bot:
                 words = questionparser.process(you)
                 segments = segment_words(words, segment_size = 3, overlap = 1)
                 replies = []
+
                 for segment in segments:
                     print("Current segment : {}".format(segment))
-                    bot = ""
-                    for keywords in questionparser.generate_variations(segment):
-                        print("Variation : {}".format(keywords))
-                        reply = self.generate_reply(keywords, reply_len=4)
-                        print(reply)
-                        if len(reply) == 1 and reply[0] == keywords:
-                            bot = "??? I can't know everything which is the beauty of life itself. ???"
-                        else:
-                            bot = convert_tuples_to_string(reply)
-                            break
-                    print("Paradox >> {}".format(bot))
+                    reply_len  = self.get_reply_len_for_segment(segment)
+                    bot = self.generate_segmented_reply(segment, reply_len = reply_len)
+                    if bot:
+                        replies.append(bot)
+                print("Paradox >> {}".format(replies))
             except (KeyboardInterrupt, EOFError) as e:
                 print("Bye. Cheers. Stay awesome...")
                 break
--- segmenter.py
+++ segmenter.py
@@ -5,6 +5,8 @@ def segment_text(text, segment_size=3, overlap=2):
         Segment the given text into  n segments
     """
     words = text.split()
+    if len(words) == 1:
+        return [ tuple(words) ]
     segments = []
     start = overlap if overlap > 0 else 0
     for i in range(start, len(words), segment_size - overlap):
@@ -17,6 +19,8 @@ def segment_words(words, segment_size=3, overlap=2):
     """
         Segment the given text into  n segments
     """
+    if len(words) == 1:
+        return [ tuple(words) ]
     segments = []
     start = overlap if overlap > 0 else 0
     for i in range(start, len(words), segment_size - overlap):
@@ -26,8 +30,9 @@ def segment_words(words, segment_size=3, overlap=2):
     return segments
 
 def main():
-    text = "what are you doing in your life"
+    text = "humans"
     segments = segment_text(text, segment_size = 4, overlap = 2)
+    #segments = segment_words(text.split(), segment_size = 4, overlap = 2)
     print(segments)
 
 
